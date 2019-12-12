37d184d6c7c3711e572d8c3eb6642c4b3b02b55f
sag754 <56234238+sag754@users.noreply.github.com>
2019-12-12 15:57:22 -0500

    Adding scripts to Main Menu and Ending Screen

---
+++ Rise of the Machine/Assets/Scripts/Powers/EndingScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndingScript : MonoBehaviour
+{
+
+    private void Update()
+    {
+        if (Input.anyKey)                  //if the player hits the spacebar
+        {
+            SceneManager.LoadScene(0);                    //go to the instructions screen
+        }
+    }
+}
---
+++ Rise of the Machine/Assets/Scripts/Powers/MainMenuScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour
+{
+
+    private void Update()
+    {
+        if (Input.anyKey)                  //if the player hits the spacebar
+        {
+            SceneManager.LoadScene(1);                    //go to the instructions screen
+        }
+    }
+}
