5c0790e6616ee853df21935a63cb39c29dc858cb
AlinaC <56270941+alunaic@users.noreply.github.com>
2019-12-13 05:51:11 -0500

    -audio triggers working on powerups. -EXTENSIVE visual tweaks on geometry for additional harmony -extensive visual edits on shaders of individual faces for player direction - game v2 built

---
+++ Rise of the Machine/Assets/Scripts/AudioCollision.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioCollision : MonoBehaviour
+{
+    public AudioClip audioClip; //create a field for the AudioSource "audio"
+    bool hasPlayed = false;
+    
+
+    // Start is called before the first frame update
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //if something hits this trigger
+    private void OnTriggerEnter(Collider other)
+    {
+        
+        if (!hasPlayed) {
+           
+
+            AudioSource audioYeah = GetComponent<AudioSource>();
+            audioYeah.clip = audioClip;
+            audioYeah.Play();
+            hasPlayed = true;
+        }
+    }
+}
---
+++ Rise of the Machine/Assets/Scripts/TriggerAudio.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerAudio : MonoBehaviour
+{
+    public AudioSource audio; //create a field for the AudioSource "audio"
+    bool hasPlayed = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audio = GetComponent<AudioSource>(); //get the AudioSource component from this gameObject
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //if something hits this trigger
+    private void OnTriggerEnter(Collider other)
+    {
+        
+        if (!hasPlayed) {
+            gameObject.GetComponent<Rigidbody>().isKinematic = true;
+            if (other.gameObject.GetComponent<Rigidbody>() == null) {
+                return;
+            }
+
+            audio.Play(); //play the audio
+            hasPlayed = true;
+        }
+    }
+}
